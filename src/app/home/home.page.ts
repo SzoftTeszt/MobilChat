50baf3ad011267db467fac2af4b945be060a37a1
Nagy János <janos@zold.lan>
2024-01-11 19:58:45 +0100

    Recapctha

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BaseService } from '../base.service';
 import { map } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,7 @@ export class HomePage {
   newMessage:any
   messages:any
   userName="Attila"
-  constructor(private base:BaseService) {
+  constructor(private base:BaseService, private auth:AuthService) {
     this.base.getMessages().snapshotChanges().pipe(
       map(
         (ch)=>ch.map(
@@ -38,5 +39,8 @@ export class HomePage {
   }
   saveMessage(message:any){
     this.base.updateMessage(message)
+  }
+  logout(){
+    this.auth.logout()
   }
 }
--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RecaptchaVerifier, getAuth } from 'firebase/auth';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,24 +10,39 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
-  phoneNo:any="+36303236954"
-  code="111111"
+  phoneNo:any=""
+  code=""
   recaptchaVerifier:any
+  recaptchaVerifierVisible:any
   user:any
-  constructor(private auth:AuthService) {
+  codeWrite=false
+  recaptcha=true
+
+  constructor(private auth:AuthService, private router:Router, private alertController:AlertController) {
     this.auth.getUser().subscribe(
       (user)=>{
         this.user=user
         console.log("SignIn Page User:", this.user)
+        if(user) this.router.navigate(['/home'])
       }
     )
    }
 
   ngOnInit() {
   }
+  ionViewWillLeave(){
+    this.recaptchaVerifierVisible.clear()
+    this.recaptchaVerifierVisible=null
+    // this.recaptchaVerifier.clear()
+    this.recaptchaVerifier=null
+  }
 
   ionViewDidEnter(){
-    this.recaptchaVerifier= new RecaptchaVerifier(
+    console.log("this.recaptchaVerifier: ",this.recaptchaVerifier)
+    console.log("this.recaptchaVerifierVisible: ",this.recaptchaVerifierVisible)
+    this.codeWrite=false
+    this.recaptcha=true
+    if (!this.recaptchaVerifier) this.recaptchaVerifier= new RecaptchaVerifier(
       getAuth(), 'recaptcha-container',
     {
       'size':'invisible',
@@ -34,14 +51,34 @@ export class SignInPage implements OnInit {
       },
       'expired-callback':()=>{
         console.log("Láthatatlan Hiba!")
+        this.recaptcha=false
       }
     })
+
+    this.recaptchaVerifierVisible= new RecaptchaVerifier(
+      getAuth(), 'recaptcha-container-visible',
+    {
+      'size':'normal',
+      'callback': ()=>{
+        console.log("Látható Oks!")
+        this.recaptcha=true
+        this.recaptchaVerifier=this.recaptchaVerifierVisible
+      },
+      'expired-callback':()=>{
+        console.log("Látható Hiba!")
+        this.recaptcha=false
+      }
+    })
+    console.log("this.recaptchaVerifier: ",this.recaptchaVerifier)
+    console.log("this.recaptchaVerifierVisible: ",this.recaptchaVerifierVisible)
   }
 
   signInWithPhone(){
-    this.auth.signInwithPhone(this.phoneNo,this.recaptchaVerifier)
+    console.log("this.recaptchaVerifier: ",this.recaptchaVerifier)
+    if (this.recaptcha) this.auth.signInwithPhone(this.phoneNo,this.recaptchaVerifier)
     .then(
       ()=>{
+        // this.codeWrite=true
         // kód
         this.codeVerifier()
       }
@@ -50,11 +87,49 @@ export class SignInPage implements OnInit {
     )
   }
 
-  codeVerifier(){
-    this.auth.verificationCode(this.code).then(
-      (res:any)=>console.log("A kód stimmel!",res)
-    ).catch(
-      (error:any)=>console.log("A kód HIBA!",error)
-    )
+  // codeVerifier(){
+  //   this.auth.verificationCode(this.code).then(
+  //     (res:any)=>{
+  //       console.log("A kód stimmel!",res)
+  //       this.code=""
+  //     }
+  //   ).catch(
+  //     (error:any)=>{
+  //       console.log("A kód HIBA!",error)
+  //       this.recaptcha=false
+  //       this.recaptchaVerifierVisible.render()
+  //       this.code=""
+  //     }
+  //   )
+  // }
+  async codeVerifier(){
+   const alert= await this.alertController.create({
+    header: 'Enter Code',
+    inputs:[
+      {
+        name:"code",
+        type:"text",
+        placeholder:"Enter your code "
+      }
+    ],
+    buttons: [
+      {text:'Enter',
+      handler:(res) =>{
+        this.auth.verificationCode(res.code)
+        .then(()=>{console.log("OK!")})
+        .catch(
+          ()=>{
+            this.recaptcha=false
+            this.recaptchaVerifierVisible.render()
+          }
+        )
+      }
+    }
+    ]
+   })
+    await alert.present()
   }
+
+
+
 }
